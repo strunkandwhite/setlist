c64b63cea99a245351325b84fc01521ddc2476bb
jlenehan <jlenehan@appnexus.com>
2017-12-03 21:27:45 -0800

    Super ugly/basic Spotify auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import * as SpotifyWebApi from 'spotify-web-api-js';
 import ImportForm from './ImportForm'
 import SongList from './SongList'
+
+const spotifyCreds = {
+	clientId: 'a453ca8acfe947bebf7bd1673e3e5f53',
+	clientSecret: '454167352426490584785aec8b9dce81'
+}
+
+let spotify = new SpotifyWebApi(spotifyCreds);
+
+spotify.setAccessToken('BQDDrHtDAOU9S_TfavtFxj0YeElOndaujsFYQsy3jHmMfXWRpeisWN_VWfiLg_F7HH1_0N53vGmZYbWAfFU');
+
+spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
+  if (err) console.error(err);
+  else console.log('Artist albums', data);
+});
 
 class App extends Component {
   render() {
@@ -24,6 +39,11 @@ class App extends Component {
 
 	handleImportFormSubmit(value) {
 		this.splitTracks(value)
+
+		spotify.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
+			if (err) console.error(err);
+			else console.log('Artist albums', data);
+		});
 	}
 
 	splitTracks(tracks) {
