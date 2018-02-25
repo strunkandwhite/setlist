520d326b46a88e6bca6f468e526aef80429b32e4
jlenehan <jlenehan@appnexus.com>
2018-02-24 17:01:18 -0800

    Implement track switching

--- src/client/components/TrackList.jsx
+++ src/client/components/TrackList.jsx
@@ -12,17 +12,19 @@ const TrackList = ({
   list,
 }) => {
   const className = (isTooLong) ? 'duration too-long' : 'duration';
+  const otherList = (list === 'set') ? 'reserve' : 'set';
+  const button = (list === 'set') ? '>' : '<';
 
   return (
-    <section className={`TrackList ${name}`}>
+    <section className={`TrackList ${list}`}>
       <h3>
-        {name}&nbsp;
+        {list}&nbsp;
         <span className={className}>
           ({formattedDuration})
         </span>
       </h3>
       <ul>
-        {listTracks.map(track => <Track key={track.id} list={list} {...track} />)}
+        {listTracks.map(track => <Track key={track.id} button={button} list={list} otherList={otherList} {...track} />)}
       </ul>
     </section>
   )
--- src/client/containers/Track.jsx
+++ src/client/containers/Track.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { changeTrackBpm, removeTrackFromList } from '../actions';
+import { changeTrackBpm, removeTrackFromList, addTrackToList } from '../actions';
 import Moment from 'moment';
 import 'moment-duration-format';
 
 import '../styles/Track.css';
 
-let Track = ({ id, artist, name, duration_ms, bpm, list, dispatch }) => {
+let Track = ({
+  id,
+  artist,
+  name,
+  duration_ms,
+  bpm,
+  list,
+  otherList,
+  button,
+  dispatch
+}) => {
   let input;
   const handleChange = e => {
     e.preventDefault();
@@ -20,6 +30,12 @@ let Track = ({ id, artist, name, duration_ms, bpm, list, dispatch }) => {
     dispatch(changeTrackBpm(id, value));
   };
 
+  const switchTrackList = e => {
+    e.preventDefault();
+    dispatch(removeTrackFromList(id, list));
+    dispatch(addTrackToList(id, otherList));
+  }
+
   return (
     <li className={`Track`}>
       <input
@@ -31,7 +47,7 @@ let Track = ({ id, artist, name, duration_ms, bpm, list, dispatch }) => {
         onChange={handleChange}
       />
       <span>({Moment.duration(duration_ms).format('m:ss')}) {artist} - {name}</span>
-      <button onClick={() => {}}>></button>
+      <button onClick={switchTrackList}>{button}</button>
       <button onClick={() => {dispatch(removeTrackFromList(id, list))}}>x</button>
     </li>
   );
@@ -43,6 +59,8 @@ Track.propTypes = {
   bpm: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   list: PropTypes.string.isRequired,
+  otherList: PropTypes.string.isRequired,
+  button: PropTypes.string.isRequired,
   duration_ms: PropTypes.number.isRequired
 };
 
