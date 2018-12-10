13b06cb566f8f80e4b32ce2c7cebf124a383596c
Jack Lenehan <jack.lenehan@gmail.com>
2018-12-09 22:43:52 -0600

    Restore uglify

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('webpack-merge')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const common = require('./webpack.common.js')
@@ -8,7 +9,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
-    minimizer: [new OptimizeCssAssetsPlugin({})],
+    minimizer: [new OptimizeCssAssetsPlugin({}), new UglifyJsPlugin({ exclude: /vendor/ })],
   },
   stats: {
     chunks: false,
